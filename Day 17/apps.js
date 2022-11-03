4a208effa1a949e5e6304d0b1a4b260111ec165d
anthonyeca <anthonyeca19esp@gmail.com>
2022-11-03 21:16:55 +0100

    Day 17

---
+++ Day 17/apps.js
@@ -0,0 +1,23 @@
+//Exercises: Level 1
+//1. Store you first name, last name, age, country, city in your browser localStorage.
+//1.
+// localStorage.setItem("firstName","ZZZZZ")
+// localStorage.setItem("lastName","XXXXX")
+// localStorage.setItem("age","30")
+// localStorage.setItem("country","Spain")
+// localStorage.setItem("city","Madrid")
+
+// Exercises: Level 2
+//1.Create a student object. The student object will have first name, last name, age, skills, country, enrolled keys and values for the keys. Store the student object in your browser localStorage.
+
+//1.
+// const student = {
+//   firstName: "ZZZZ",
+//   lastName: "XXXX",
+//   age: "30",
+//   skills: ["HTML", "CSS", "JAVASCRIPT"],
+//   country: "Spain",
+// }
+
+// let studentJSON = JSON.stringify(student, undefined, 4)
+// localStorage.setItem("student", studentJSON)
